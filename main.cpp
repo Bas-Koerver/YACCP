acb15ee3ea63a379bf02c279d0269725351f9bc1
Bas Körver <git@bas.xn--krver-jua.it>
2025-10-26 14:16:34 +0100

    Added ChArUco board creation

--- main.cpp
+++ main.cpp
@@ -1,21 +1,34 @@
 #include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/calib3d.hpp>
 #include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/objdetect/charuco_detector.hpp>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+static inline void createBoard()
+{
+    int squaresX = 5;
+    int squaresY = 7;
+    float squareLength = 0.04f; // in meters
+    float markerLength = 0.02f; // in meters
+    int margins = squareLength - markerLength;
+
+    int borderBits = 1;
+
+    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+    cv::aruco::CharucoBoard board(cv::Size(squaresX, squaresY), squareLength, markerLength, dictionary);
+
+    cv::Mat boardImage;
+    cv::Size imageSize;
+    imageSize.width = squaresX * squareLength + 2 * margins;
+    imageSize.height = squaresY * squareLength + 2 * margins;
+    board.generateImage(imageSize, boardImage, margins, borderBits);
+
+    cv::imwrite("board.png", boardImage);
+}
+
 int main() {
-
-    cv::imread("image.jpg");
-
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
-
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
-
-    return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+    createBoard();
 }
